5054c3b50f2810562c40b694589ab5d80b7cf52c
J.R Perez <jrbaprz@gmail.com>
2022-12-23 10:34:37 -0500

    Updated Margins, added Sanity Studio

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,17 +6,6 @@ module.exports = {
   ],
   theme: {
     extend: {},
-  },
-  plugins: [],
-};
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {},
     fontFamily: {
       akkurat: ["Akkurat", "sans-serif"],
       akkuratlight: ["Akkurat Light", "sans-serif"],
