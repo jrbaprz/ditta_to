5335020dee6188e4b7bf7531aa6cbef4de1fe20b
J.R Perez <dev@jrbaprz.com>
2023-03-27 10:25:10 -0400

    Fixed GTM

--- pages/_app.js
+++ pages/_app.js
@@ -1,3 +1,5 @@
+import Head from "next/head";
+import Script from "next/script";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import "../styles/globals.css";
@@ -5,6 +7,24 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <title>Ditta Artigianale Torotno</title>
+      </Head>
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        dangerouslySetInnerHTML=
+        {{
+          __html: `(function (w, d, s, l, i) {
+              w[l] = w[l] || [];
+              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+              var f = d.getElementsByTagName(s)[0],
+                j = d.createElement(s),
+                dl = l != "dataLayer" ? "&l=" + l : "";
+              j.async = true;
+              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+              f.parentNode.insertBefore(j, f);
+            })(window, document, "script", "dataLayer", "GTM-N9RHGPN");`,
+        }}
+      </Script>
       <Header />
       <Component {...pageProps} />
       <Footer />
--- pages/_document.js
+++ pages/_document.js
@@ -3,32 +3,13 @@ import { Html, Head, Main, NextScript, Script } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head>
-        {/* Google Tag Manager */}
-        <Script
-          id="GTM"
-          dangerouslySetInnerHTML={{
-            __html: `(function (w, d, s, l, i) {
-              w[l] = w[l] || [];
-              w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
-              var f = d.getElementsByTagName(s)[0],
-                j = d.createElement(s),
-                dl = l != "dataLayer" ? "&l=" + l : "";
-              j.async = true;
-              j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
-              f.parentNode.insertBefore(j, f);
-            })(window, document, "script", "dataLayer", "GTM-N9RHGPN");`,
-          }}
-        />
-      </Head>
       <body>
         <Main />
         <NextScript />
         {/* Google Tag Manager (noscript) */}
         <noscript
           dangerouslySetInnerHTML={{
-            __html: `iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9RHGPN"
-  height="0" width="0" style="display:none;visibility:hidden"/>`,
+            __html: `iframe src="https://www.googletagmanager.com/ns.html?id=GTM-N9RHGPN" height="0" width="0" style="display:none;visibility:hidden"/>`,
           }}
         />
       </body>
