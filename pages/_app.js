29726d5f26b75e5393886315c794023015d0cd61
J.R Perez <dev@jrbaprz.com>
2023-03-29 18:33:37 -0400

    Updates

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Script from "next/script";
 import Footer from "../components/footer";
 import Header from "../components/header";
 import "../styles/globals.css";
@@ -7,9 +5,6 @@ import "../styles/globals.css";
 export default function App({ Component, pageProps }) {
   return (
     <>
-      <Head>
-        <title>Ditta Artigianale Toronto</title>
-      </Head>
       <Header />
       <Component {...pageProps} />
       <Footer />
