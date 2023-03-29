d7a3f6e5bb399c3cbaf12e8307abbe30d58bdfe5
J.R Perez <dev@jrbaprz.com>
2023-03-29 18:39:45 -0400

    Updates

--- pages/_document.js
+++ pages/_document.js
@@ -1,11 +1,12 @@
-import { Html, Head, Main } from "next/document";
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
+      <Head />
       <body>
-        <Head />
         <Main />
+        <NextScript />
       </body>
     </Html>
   );
